686a3837b8a88828247ffc19c598f9e198635721
dydtjr1128 <cys_star@naver.com>
2018-12-08 15:49:40 +0900

    Change structures

--- ScreenCaptureSample/src/main/java/dydtjr1128/ScreenCapture.java
+++ ScreenCaptureSample/src/main/java/dydtjr1128/ScreenCapture.java
@@ -6,6 +6,8 @@ import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
@@ -18,17 +20,19 @@ import java.util.concurrent.SynchronousQueue;
 
 
 public class ScreenCapture extends JFrame {
-    private JLabel FPSlabel;
-    private int FPScount = 0;
+    private double FPScount = 0;
     private int screenWidth, screenHeight;
     int buffersize = 1;
     byte imgbyte[][] = new byte[buffersize][];
-    SynchronousQueue<byte [] > imageBlockingQueue2 = new SynchronousQueue<>();
+    BufferedImage prevImg;
+    BufferedImage img;
+    SynchronousQueue<byte[]> imageBlockingQueue2 = new SynchronousQueue<>();
     BlockingQueue<byte[]> imageBlockingQueue = new ArrayBlockingQueue<>(3);
 
     public ScreenCapture() {
         setTitle("screen");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
         setLayout(null);
         //this.setAlwaysOnTop(true);
         ScreenPanel sp = new ScreenPanel();
@@ -37,9 +41,8 @@ public class ScreenCapture extends JFrame {
         setVisible(true);
 
         Thread th = new Thread(sp);
+        th.setPriority(Thread.MAX_PRIORITY);
         th.start();
-        FPSCheckThread fps = new FPSCheckThread();
-        fps.start();
     }
 
     class ScreenPanel extends JPanel implements Runnable {
@@ -50,10 +53,6 @@ public class ScreenCapture extends JFrame {
         public ScreenPanel() {
             screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
             screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-            FPSlabel = new JLabel("FPS : " + Integer.toString(FPScount));
-            FPSlabel.setFont(new Font("�������", Font.BOLD, 20));
-            FPSlabel.setBounds(10, 10, 100, 50);
-            add(FPSlabel);
             addMouseListener(new MouseListener() {
                 public void mouseClicked(MouseEvent e) {
                     if (e.getClickCount() == 1 && e.getButton() == e.BUTTON3) {
@@ -94,59 +93,45 @@ public class ScreenCapture extends JFrame {
             }
             screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
             screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-            image = robot.createScreenCapture(new Rectangle(0, 0, screenWidth, screenHeight));
-
-            repaint();
+            javax.swing.Timer timer = new javax.swing.Timer(1000 / 60, e -> {
+                //System.out.println("@@@@@@@@@@@@@@");
+                repaint();
+            });
+            timer.start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            img = toBufferedImage(imageBlockingQueue2.take());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
         }
 
         @Override
         protected void paintComponent(Graphics g) {
-
             super.paintComponent(g);
-            try {
-                FPScount++;
-                BufferedImage ii = toBufferedImage(imageBlockingQueue2.take());
-                g.drawImage(ii, 0, 0, getWidth(), getHeight(), this);
-                //byte[] imageBytes = ((DataBufferByte) ii.getData().getDataBuffer()).getData();
-                //System.out.println(imageBytes.length);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            //if (prevImg != img) {
 
-
+                //스크린X : 스크린Y = 화면X : 화면 Y
+                //getWidth()*screenHeight/screenWidth = y;
+                //System.out.println(" " + getWidth() + " " + screenHeight);
+                int panelWidth = getWidth();
+                int height = panelWidth * screenHeight / screenWidth;
+                g.drawImage(img, 0, (getHeight() / 2) - (height / 2), panelWidth, height, this);
+                g.setFont(new Font("한컴 윤고딕 250", Font.BOLD, 15));
+                g.drawString(("FPS : " + String.format("%.2f",FPScount)), panelWidth / 2 - 10, 30);
+                prevImg = img;
+            //}
+            //g.drawOval(100,100,300,300);
         }
 
-        /*private BufferedImage screenShot(){
-            //Creating an rbg array of total pixels
-            int[] pixels = new int[screenWidth * screenHeight];
-            int bindex;
-            // allocate space for RBG pixels
-            ByteBuffer fb = ByteBuffer.allocateDirect(screenWidth * screenHeight * 3);
-
-            // grab a copy of the current frame contents as RGB
-            GL11.glReadPixels(0, 0, screenWidth, screenHeight, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, fb);
-
-            BufferedImage imageIn = new BufferedImage(screenWidth, screenHeight,BufferedImage.TYPE_INT_RGB);
-            // convert RGB data in ByteBuffer to integer array
-            for (int i=0; i < pixels.length; i++) {
-                bindex = i * 3;
-                pixels[i] =
-                    ((fb.get(bindex) << 16))  +
-                    ((fb.get(bindex+1) << 8))  +
-                    ((fb.get(bindex+2) << 0));
-            }
-            //Allocate colored pixel to buffered Image
-            imageIn.setRGB(0, 0, screenWidth, screenHeight, pixels, 0 , screenWidth);
-
-            //Creating the transformation direction (horizontal)
-            AffineTransform at =  AffineTransform.getScaleInstance(1, -1);
-            at.translate(0, -imageIn.getHeight(null));
-
-            //Applying transformation
-            AffineTransformOp opRotated = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
-            BufferedImage imageOut = opRotated.filter(imageIn, null);
-            return imageOut;
-        }*/
         public void run() {
             Rectangle rect = new Rectangle(0, 0, screenWidth, screenHeight);
             double avg = 0;
@@ -160,10 +145,12 @@ public class ScreenCapture extends JFrame {
                     //image = getScaledImage(image, 1920, 1080);
 
                     //9~11frame
-                    imageBlockingQueue2.add(toByteArray(image));
-                    avg += 1/((System.currentTimeMillis() - s) / (double) 1000);
-                    System.out.println(imageBlockingQueue2.size() + " " + avg / count++);
-                    repaint();
+                    imageBlockingQueue2.put(toByteArray(image));
+                    avg += 1 / ((System.currentTimeMillis() - s) / (double) 1000);
+                    FPScount = avg / count++;
+                    //System.out.println(imageBlockingQueue2.size() + " " + avg / count++);
+                    //repaint();
+
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -195,76 +182,8 @@ public class ScreenCapture extends JFrame {
         return background;
     }
 
-    /*class ImgDoubleBufferTh extends Thread {
-        BufferedImage bufferimage;
-
-        public void run() {
-            int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-            int screennHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-            BufferedImage screenImage;
-            Robot robot = null;
-            try {
-                robot = new Robot();
-            } catch (AWTException e) {
-            }
-
-            Rectangle rect = new Rectangle(0, 0, screenWidth, screennHeight);
-            int ff = 1;
-            long starttime, endtime;
-            while (true) {
-                starttime = System.nanoTime();
-                bufferimage = robot.createScreenCapture(rect);//JNAScreenShot.getScreenshot(rect);//robot.createScreenCapture(rect);
-                endtime = System.nanoTime() - starttime;
-                System.out.println("capture  " + endtime);
-                bufferimage = getScaledImage(bufferimage, 1920, 1080);
-                byte[] imageByte = ((DataBufferByte) bufferimage.getRaster().getDataBuffer()).getData();
-
-                try {
-                    byte[] bbb = Snappy.compress(imageByte);
-                    b.addElement(bbb);
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-			*//*	starttime = System.nanoTime();
-				bb.addElement(bufferimage);
-				endtime = System.nanoTime() -starttime;
-				System.out.println("add  " + endtime);	*//*
-
-
-            }
-
-        }
-    }*/
-
-    class FPSCheckThread extends Thread {
-        int sum = 0;
-        double avg = 0;
-        int roop = 0;
-
-        public void run() {
-            while (true) {
-                try {
-
-                    sleep(1000);
-                    sum += FPScount;
-                    roop++;
-                    avg = (double) sum / roop;
-                    avg = Double.parseDouble(String.format("%.3f", avg));
-
-                    FPSlabel.setText("FPS : " + Integer.toString(FPScount) + " " + Double.toString(avg));
-                    //repaint();
-                    System.out.println("FPS : " + FPScount + " " + avg);
-
-                    FPScount = 0;
-
-                } catch (InterruptedException e) {
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
+        System.setProperty("sun.awt.noerasebackground", "true");
         new ScreenCapture();
     }
 }
